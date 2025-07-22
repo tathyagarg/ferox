3addbe84b5776dbf3d8c84ead2c89d95bc2c5a68
Tathya <coding.tathya@gmail.com>
2025-07-23 03:31:26 +0530

    refactor: Migrated away from fermium

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,109 @@
-use env_logger;
-use std::ffi::CString;
+use log::{info, warn};
+use std::sync::Arc;
 
-pub mod window;
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowId},
+};
 
-pub fn main() {
+mod input;
+mod state;
+
+#[derive(Default)]
+struct App {
+    state: Option<state::State>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        // Create window object
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        let state = pollster::block_on(state::State::new(window.clone()));
+        self.state = Some(state);
+
+        window.request_redraw();
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        let state = self.state.as_mut().unwrap();
+
+        match event {
+            WindowEvent::CloseRequested => {
+                info!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                state.get_window().request_redraw();
+            }
+            WindowEvent::Resized(size) => {
+                // Reconfigures the size of the surface. We do not re-render
+                // here as this event is always followed up by redraw request.
+                state.resize(size);
+            }
+            WindowEvent::KeyboardInput {
+                event,
+                is_synthetic: false,
+                ..
+            } => match event.physical_key {
+                PhysicalKey::Code(key_code) => {
+                    info!("KeyboardInput: {:?}", key_code);
+                    if let Some(action) = input::make_key_action(key_code) {
+                        if let Err(e) = input::handle_action(action, self, event_loop) {
+                            info!("Error handling action: {}", e);
+                        }
+                    }
+                }
+                PhysicalKey::Unidentified(..) => {
+                    warn!("Unknown key pressed");
+                }
+            },
+            _ => (),
+        }
+
+        // match event {
+        //     WindowEvent::CloseRequested => {
+        //         println!("The close button was pressed; stopping");
+        //         event_loop.exit();
+        //     }
+        //     WindowEvent::RedrawRequested => {
+        //         state.render();
+        //         // Emits a new redraw requested event.
+        //         state.get_window().request_redraw();
+        //     }
+        //     WindowEvent::Resized(size) => {
+        //         // Reconfigures the size of the surface. We do not re-render
+        //         // here as this event is always followed up by redraw request.
+        //         state.resize(size);
+        //     }
+        //     WindowEvent::KeyboardInput {
+        //         event,
+        //         is_synthetic: false,
+        //         ..
+        //     } => {
+        //         info!("KeyboardInput: {:?}", event.physical_key)
+        //     }
+        //     WindowEvent::MouseInput { state, button, .. } => {
+        //         info!("MouseInput: {:?} {:?}", state, button);
+        //     }
+        //     _ => (),
+        // }
+    }
+}
+
+fn main() {
     env_logger::init();
 
-    let user_app = window::UserApp {
-        height: 600,
-        width: 800,
-        title: CString::new("Ferox").unwrap().into_raw(),
-    };
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-    window::run(user_app);
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
 }
